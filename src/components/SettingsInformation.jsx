9d3d445abba80040c72f3d4a44863ef63fb7d1da
Azuyamat <blaneyderek@gmail.com>
2023-11-29 02:07:25 -0500

    Updated updater

--- src/components/SettingsInformation.jsx
+++ src/components/SettingsInformation.jsx
@@ -1,8 +1,12 @@
 import {getVersion, getTauriVersion} from "@tauri-apps/api/app";
-import {checkUpdate} from "@tauri-apps/api/updater";
-import {useEffect, useState} from "react";
+import {checkUpdate, installUpdate, onUpdaterEvent} from "@tauri-apps/api/updater";
+import {useContext, useEffect, useState} from "react";
+
+import styles from "../styles/components/Settings.module.css";
+import {ToastContext} from "../contexts/ToastContext.jsx";
 
 export default function SettingsInformation() {
+    const {showToast} = useContext(ToastContext);
     const [information, setInformation] = useState({
         version: "",
         tauriVersion: "",
@@ -13,21 +17,34 @@ export default function SettingsInformation() {
         async function updateInformation(){
             const version = await getVersion();
             const tauriVersion = await getTauriVersion();
-            const update = await checkUpdate().catch(() => "No update available");
+            const update = await checkUpdate();
+            console.log(update);
             setInformation({
                 version: version,
                 tauriVersion: tauriVersion,
-                update: update || "No update available"
+                update: update?.shouldUpdate || "You are already up to date!"
             });
         }
         updateInformation();
     }, []);
+
+    onUpdaterEvent((event) => {
+        const status = event.status;
+        const toastMessage =
+            status === "DONE" ? {message: "Update complete!", type: "success"} :
+            status === "ERROR" ? {message: "Update failed!", type: "error"} :
+            status === "PENDING" ? {message: "Update in progress...", type: "info"} : null;
+        if (toastMessage) showToast(toastMessage.message, toastMessage.type);
+    });
 
     return (
         <li>
             <p>Mia version <span className={"highlight"}>{information.version}</span></p>
             <p>Tauri version <span className={"highlight"}>{information.tauriVersion}</span></p>
             <p>Update <span className={"highlight"}>{information.update}</span></p>
+            {information?.update?.shouldUpdate && (
+                <button className={styles.updateButton} onClick={installUpdate}>Update</button>
+            )}
         </li>
     )
 }
