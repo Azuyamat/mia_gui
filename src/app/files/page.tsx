5667d186d00c71659659685daf74fbcfb466f39a
Azuyamat <discordazu@gmail.com>
2025-03-23 02:01:21 -0400

    attempt to fix file explorer flashing

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import FileExplorer from "@/components/file-explorer/FileExplorer.tsx";
 import useCurrentPath from "@/hooks/useCurrentPath.ts";
 
 export default function Page(): React.ReactElement {
@@ -9,7 +8,8 @@ export default function Page(): React.ReactElement {
 
     return (
         <div>
-            <FileExplorer path={path} />
+            {path}
+            {/*<FileExplorer path={path} />*/}
         </div>
     );
 }
