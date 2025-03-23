485ea47e9a43a13f74ee9d2b8adc3be7877723aa
Azuyamat <blaneyderek@gmail.com>
2025-03-23 13:45:37 -0400

    fixed suspense boundary error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,17 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
     baseDirectory: import.meta.dirname,
+    packageManager: "npm",
 });
 
 const eslintConfig = [
     ...compat.config({
         extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+        settings: {
+            next: {
+                rootDir: "src-next",
+            },
+        },
     }),
 ];
 
--- src-next/app/files/page.tsx
+++ src-next/app/files/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import useCurrentPath from "@/hooks/useCurrentPath.ts";
 import FileExplorer from "@/components/file-explorer/FileExplorer.tsx";
 
 export default function Page(): React.ReactElement {
+    return (
+        <Suspense>
+            <Content />
+        </Suspense>
+    );
+}
+
+function Content(): React.ReactElement {
     const { path } = useCurrentPath();
 
     return (
